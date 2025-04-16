135a9bea9bb13f366613cf20f366d34a6d8c8f4a
Nocito Davide <Davide.Nocito@swisscom.com>
2025-04-16 10:53:59 +0200

    added homepage und Navbar folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Homepage from './components/Homepage/Homepage'
 
 const App = () => {
 
   return (
-    <div></div>
+    <Homepage />
   )
 }
 
