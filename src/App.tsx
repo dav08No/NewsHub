ef5f84fe4ee3e9fc1f9b6a6aa8975bc1ebbf9ce4
Nigg Joel <Joel.Nigg@swisscom.com>
2025-04-16 13:34:57 +0200

    Grundstrucktur

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/Navbar/Navbar";
 import Homepage from "./components/Homepage/Homepage";
 import SucheFilter from "./components/SucheFilter/SucheFilter";
 import AboutUs from "./components/AboutUs/AboutUs";
@@ -11,10 +11,9 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Homepage />
+      
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/homepage" element={<Homepage />} />
         <Route path="/suchefilter" element={<SucheFilter />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/kontakt" element={<Kontakt />} />
--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,9 +1,31 @@
+import './AboutUs.css'
 const AboutUs = () => {
     return (
-      <div className="about-container">
-        <h1>Über uns</h1>
-        <p>Test</p>
-      </div>
+    <>
+        <div className="WebsiteTitle">
+            <h1>About Us</h1>
+        </div>
+        <br></br>
+        <div className="UnserTeam">
+            <h3>Unser Team</h3>
+        </div>
+
+        <div className="UnserProjekt"> 
+            <h3>Unser Projekt</h3>
+        </div>
+        
+        <div className="TextTeam">
+            <p>
+                Unser Team besteht aus 5 Personen. 
+            </p>
+        </div>
+
+        <div className="TextProjekt">
+            <p>
+                Unsere Idee unseres Projektes war, eine Website zu erstellen, auf der man im Allgemeinen die wichtigsten Nachrichten der letzten Zeit anschauen kann. Wir haben dafür die API News API verwendet, um die 
+            </p>
+        </div>
+    </>
     );
   };
   
