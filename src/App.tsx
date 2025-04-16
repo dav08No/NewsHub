ac6174aad1f805318b6479cbd92370fc832e3ad9
Spiri Fabian <Fabian.Spiri@swisscom.com>
2025-04-16 13:29:23 +0200

    Final Push Navbar mit CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Homepage from "./components/Homepage/Homepage";
 import SucheFilter from "./components/SucheFilter/SucheFilter";
--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div>
-      <h1>HEllo World</h1>
+      <h1>Hello World</h1>
     </div>
   )
 }
