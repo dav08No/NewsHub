d2c7b0f7aef9c4a678069431073e52b06c8ebab8
Nocito Davide <davide.nocito@swissom.com>
2025-04-17 14:59:07 +0200

    Detailview finishd NO CSS

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SucheFilter from "./components/SucheFilter/SucheFilter";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Kontakt from "./components/Kontakt/Kontakt";
 import Anmelden from "./components/Anmelden/Anmelden";
+import Detailview from "./components/Detailview/Detailview";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/kontakt" element={<Kontakt />} />
         <Route path="/anmelden" element={<Anmelden />} />
+        <Route path="/article/:id" element={<Detailview />} />
       </Routes>
     </Router>
   );
