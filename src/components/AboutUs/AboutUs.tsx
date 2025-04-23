c40c48a3c81be41b31e54ce40eda9fc4d1a99ba4
Leon Gjidoda <leon.gjidoda@swisscom.com>
2025-04-23 12:28:04 +0200

    Changed the About Us Page

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,33 +1,50 @@
-import './AboutUs.css'
+import "./AboutUs.css";
+
 const AboutUs = () => {
-    return (
+  return (
     <>
-        <br></br>
-        <div className="WebsiteTitle">
-            <h1>Über Uns</h1>
+      <div className="container">
+        <h1 className="title-about">Über uns</h1>
+        <div className="textTeam">
+          <h3 className="title-ourTeam">Unser Team</h3>
+          <p>
+            Unser Team besteht aus fünf leidenschaftlichen Programmierern, die
+            gemeinsam diese Newswebseite entwickelt haben. Mit Herzblut,
+            Kreativität und technischem Know-how haben wir eine Plattform
+            geschaffen, die Informationen modern und zugänglich macht.
+          </p>
+          <div className="pepole">
+            <div className="description">
+              <img className="profile" src="profile.jpg" />
+              <h3>Leon Gjidoda</h3>
+              <p>16 Jahre 1.Lehrjahr</p>
+            </div>
+            <div className="description">
+              <img className="profile" src="profile.jpg" />
+            </div>
+            <div className="description">
+              <img className="profile" src="profile.jpg" />
+            </div>
+            <div className="description">
+              <img className="profile" src="profile.jpg" />
+            </div>
+            <div className="description">
+              <img className="profile" src="profile.jpg" />
+            </div>
+          </div>
         </div>
-        <br></br><br></br><br></br>
-        <div className="UnserTeam">
-            <h3>Unser Team</h3>
-        </div>
+        <div className="textProjekt">
+          <h3 className="title-ourProject">Unser Projekt</h3>
 
-        <div className="UnserProjekt"> 
-            <h3>Unser Projekt</h3>
+          <p>
+            Unsere Idee unseres Projektes war, eine Website zu erstellen, auf
+            der man im Allgemeinen die wichtigsten Nachrichten der letzten Zeit
+            anschauen kann. Wir haben dafür die API News API verwendet, um die
+          </p>
         </div>
-        
-        <div className="TextTeam">
-            <p>
-                Unser Team besteht aus 5 Personen. 
-            </p>
-        </div>
-
-        <div className="TextProjekt">
-            <p>
-                Unsere Idee unseres Projektes war, eine Website zu erstellen, auf der man im Allgemeinen die wichtigsten Nachrichten der letzten Zeit anschauen kann. Wir haben dafür die API News API verwendet, um die 
-            </p>
-        </div>
+      </div>
     </>
-    );
-  };
-  
-  export default AboutUs;
+  );
+};
+
+export default AboutUs;
